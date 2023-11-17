f2cd6966bfde651011f54adabb1032c33edc41c0
AdrianSalvador456 <adriansalvador456@gmail.com>
2023-11-17 12:11:16 -0700

    [17-11-2023] Modificaciones de bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from './app.component';
 import { PersonComponent } from './person/person.component';
+import bootstrap from '../main.server';
+bootstrap: [AppComponent]
 
 @NgModule({
     declarations: [
--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core'
+import { Component,Input, OnInit } from '@angular/core'
 
 @Component({
     selector: 'app-person',
     templateUrl: './person.component.html',
-    styleUrls: ['./person.component.css']
+    styleUrls: ['./person.component.css'],
 })
 export class PersonComponent implements OnInit {
     
-    constructor() {  }
+    @Input() name:string = "";
+    @Input() lastname:string = "";
+    @Input() age:number = 0;
 
+    constructor() {  }
+    
     ngOnInit() {
         
     }
